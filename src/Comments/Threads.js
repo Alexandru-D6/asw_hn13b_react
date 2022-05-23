72be34efe80719f821ec2d69bf5a495100ee76ad
Víctor Asenjo <victorasenj@gmail.com>
2022-05-23 13:30:58 +0200

    task #131 Fer la crida Threads a l'api i fer display adecuat del json
    task #131 Fer la crida Threads a l'api i fer display adecuat del json

--- src/Comments/Threads.js
+++ src/Comments/Threads.js
@@ -23,7 +23,15 @@ class Threads extends Component {
       items: [],
       isLoaded: false,
       comment: [],
+      titles_submissions:[],
       userUpvoted: [],
+      status: 200,
+      error: "",
+      message: "",
+      isLoaded2: false,
+      status2: 200,
+      error2: "",
+      message2: "",
     }
   }
   componentDidMount() {
@@ -32,33 +40,63 @@ class Threads extends Component {
       .then(json => {
         this.setState({
           isLoaded: true,
-          items: json.thread,
+          items: json.comments,
+          titles_submissions: json.titles_submissions,
+          status: json.status,
+          error: json.error,
+          message: json.message,
         })
+        return json
       })
+    const requestOpt = {
+      method: 'GET',
+      headers: {
+          'X-API-KEY': process.env.REACT_APP_API_KEY,
+          'accept': 'application/json',
+          'Content-Type': 'application/json'
+      },
+    }
+    fetch('https://serene-ridge-36448.herokuapp.com/API/v1.0/users/upvotedComments/', requestOpt)
+        .then(res => res.json())
+        .then(json => {
+            var temp = []
+        
+            json.comments.map((comment) => (
+                temp.push(comment.id)
+            ))
+            this.setState({
+                isLoaded2: true,
+                userUpvoted: temp,
+                status2: json.status,
+                error2: json.error,
+                message2: json.message,
+            })
+        })
   }
 
   render() {
     var{ isLoaded} = this.state
 
-    if (!isLoaded) {
+    if (!isLoaded || !this.state.isLoaded2) {
     return <div>Loading....</div>
     }else {
         return ( //html
             <div className="Threads" align="center">
                 
                 <DisplayErrorsNoTable status={this.state.status} error={this.state.error} message={this.state.message}/>
-
-                <table width={"85%"}>                        
+                
+                <table width={"85%"}> 
+                {this.state.items.map(item => (                       
                     <Comment 
-                    comment={this.state.comment} 
-                    userUpvoted={this.state.userUpvoted.find(data => data === this.state.comment.id)} 
+                    comment={item} 
+                    userUpvoted={this.state.userUpvoted.find(data => data === item.id)} 
                     />
+                  ))}
                 </table>
 
             </div>
         );
-    }
-    
-}
+    }    
+  }
 }
 export default Threads;
