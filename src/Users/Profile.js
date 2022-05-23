8e8e33aaebc4e61430204e5d0bde4520975f1125
peilinni16 <95467823+peilinni16@users.noreply.github.com>
2022-05-23 21:04:41 +0200

    profile advances

--- src/Users/Profile.js
+++ src/Users/Profile.js
@@ -1,48 +1,100 @@
 import React, {Component} from 'react';
-import '../CSS/App.css';
+import '../CSS/Profile.css';
+import moment from "moment";
+
 
 import { Routes, Route, useNavigate} from "react-router-dom";
+
 
 class Profile extends Component {
     constructor(props) {
         super(props);
         this.state = {
-          items: [],
+          item: [],
           isLoaded: false,
+          aboutValue: "",
         }
     }
+
     componentDidMount() {
-        const requestOptions = {
-            method: 'GET',
-            headers: {'name': 'ElectrickeOfficial'}
-        }
-        fetch('https://serene-ridge-36448.herokuapp.com/API/v1.0/user/'+'ElectrikeOfficial', requestOptions)
-          .then(res => res.json())
-          .then(json => {
-            this.setState({
-              isLoaded: true,
-              items: json.user,
-            })
+      /// Redirecciona de una vista a otra  
+      /* window.location.replace(window.location.origin + "/News"); */
+      /// process.env.REACT_APP_API_NAME = nombre del usuario
+      /// process.env.REACT_APP_API = api key
+      fetch('https://serene-ridge-36448.herokuapp.com/API/v1.0/user/'+ process.env.REACT_APP_API_KEY_NAME)
+        .then(res => res.json())
+        .then(json => {
+          this.setState({
+            isLoaded: true,
+            item: json.user,
+            aboutValue: json.user.about,
           })
+        })
     }
     render() {
 
-        var{ isLoaded, items} = this.state
+        var{ isLoaded, item} = this.state
     
         if (!isLoaded) {
-          return <div>Loading....</div>
+          return <table>
+                    <tbody>Loading....</tbody>
+                  </table>
         }else {
           return (
-            <div className="App">
-              
-              <ul>
-                {items.map(item => (
-                  <li key={item.id}>
-                    {item.title}
-                  </li>
-                ))}
-              </ul>
-            </div>
+            <body>
+              <table border="0" cellPadding="0" width="85%" bgcolor="#f6f6ef">
+                <tbody>
+                  <tr className="athing">
+                    <td valign='top'>user: </td>
+                    <td>{item.name}</td>
+                  </tr>
+                  <tr>
+                    <td valign='top'>created: </td>
+                    <td>{moment.utc(item.created_at).local().startOf('seconds').fromNow()}</td>
+                  </tr>
+                  <tr>
+                    <td valign='top'>about: </td>
+                    <td>
+                      <textarea cols="60" rows="5" wrap="virtual" name="about" defaultValue={this.state.about}>{item.about}</textarea>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td>
+                      <a>
+                        <u>submissions</u>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td>
+                      <a>
+                        <u>comments</u>
+                      </a>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td></td>
+                    <td>
+                      <a>
+                        <u>upvoted submissions</u>
+                      </a>
+                      &nbsp;
+                      /
+                      &nbsp;
+                      <a>
+                        <u>comments</u>
+                        &nbsp;
+                      </a>
+                      <span>(private)</span>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <br></br>
+              <input type="submit" value= "update"></input>
+            </body>
           );
         }
         
